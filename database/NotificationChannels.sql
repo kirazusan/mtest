1d2bc70d1e4b478837a5943d889380583d954ccc
kirazusan <116509485+kirazusan@users.noreply.github.com>
2025-06-16 12:48:44 +0200

    Delete database directory

--- database/NotificationChannels.sql
+++
@@ -1,9 +0,0 @@
-
-
-CREATE TABLE NotificationChannels (
-    Id INT PRIMARY KEY NOT NULL,
-    Name VARCHAR(255) NOT NULL,
-    Description VARCHAR(500) NOT NULL,
-    UNIQUE (Name),
-    INDEX idx_Name (Name)
-);
